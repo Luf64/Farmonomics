f2b2e0f6fd6f88c5afce9cec2ff7b29c71c3b110
Phua Yew Zhian <yewzhianphua@gmail.com>
2026-04-10 18:04:41 +0800

    Global code & Esc button & Game Title

---
+++ scrips/global.gd
@@ -0,0 +1,20 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#@warning_ignore("unused_parameter")
+func _process(delta: float) -> void:
+	pass
+
+#clock 
+#calendar
+#inventory JSON system
+#player status global/local
+#level system for unlocking new equipment and seeds
--- scrips/room_0.gd
+++ scrips/room_0.gd
@@ -5,8 +5,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-
+	
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
@@ -21,3 +21,4 @@ func _on_settings_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+	
