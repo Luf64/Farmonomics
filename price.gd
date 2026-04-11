6388f9bce5e617e371efa918fd211cb0785c13be
Hoong Siang Eng <xiangdidi0@gmail.com>
2026-04-12 01:21:51 +0800

    coin and price

---
+++ price.gd
@@ -0,0 +1,16 @@
+extends Node
+
+# player money
+var money = 100.0
+
+# player bag
+var inventory = {
+	"Tomato": 5,
+	"Corn": 0
+}
+
+# market data
+var market_data = {
+	"Tomato": {"base_price": 10.0, "stock": 50, "ideal": 50},
+	"Corn": {"base_price": 20.0, "stock": 30, "ideal": 30}
+}
