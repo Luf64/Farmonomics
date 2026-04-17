a03f8f6daf3b1d0272f5a61aecb6421db6bc73f2
Hoong Siang Eng <xiangdidi0@gmail.com>
2026-04-17 22:43:06 +0800

    add collision shape block

--- scripts/price.gd
+++ scripts/price.gd
@@ -6,11 +6,17 @@ var money = 100.0
 # player bag
 var inventory = {
 	"Tomato": 5,
-	"Corn": 0
+	"Corn": 0,
+	"Apple":0,
+	"Oringe":0,
+	"Potato":0
 }
 
 # market data
 var market_data = {
 	"Tomato": {"base_price": 10.0, "stock": 50, "ideal": 50},
-	"Corn": {"base_price": 20.0, "stock": 30, "ideal": 30}
+	"Corn": {"base_price": 20.0, "stock": 30, "ideal": 30},
+	"Appel": {"base_price": 30.0, "stock": 40, "ideal": 40},
+	"Oringe": {"base_price": 30.0, "stock": 40, "ideal": 40},
+	"Potato": {"base_price": 15.0, "stock": 50, "ideal": 50}
 }
