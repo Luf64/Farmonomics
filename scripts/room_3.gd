60e7ad5428fe74e33eb0145a42b8a272f7a4e9ed
Aw Wei Jun <weijunaw@gmail.com>
2026-04-18 16:03:01 +0800

    finish up room 3 collision blocks and fix some tp to room 0.1 bug

--- scripts/room_3.gd
+++ scripts/room_3.gd
@@ -11,7 +11,7 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_tp_to_room_0_1_body_entered(body: Node2D) -> void:
 	if body.name == "player":
 		get_tree().change_scene_to_file("res://rooms/room0.1.tscn")
 	pass 
