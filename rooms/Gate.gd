3b4a6e981ea23f35e13abb45b167f7cbb20fb746
Phua Yew Zhian <yewzhianphua@gmail.com>
2026-04-20 14:30:39 +0800

    Added gates and debuging

--- rooms/Gate.gd
+++ rooms/Gate.gd
@@ -1,6 +1,6 @@
 extends Area2D
-var Room0_1 = "res://room0.1.tscn"
 
 func _on_body_entered(body: Node2D) -> void:
-	if name == "Area":
-		get_tree().change_scene_to_file(Room0_1)
+	if body.name == "player":
+		get_tree().change_scene_to_file("res://rooms/room_1.tscn")
+	pass 
