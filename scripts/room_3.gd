83ada31b8522e8ec0a1e272e3221a7f4deea5f1f
Aw Wei Jun <weijunaw@gmail.com>
2026-04-20 14:25:22 +0800

    pre npc interaction testing

--- scripts/room_3.gd
+++ scripts/room_3.gd
@@ -1,5 +1,6 @@
 class_name room_3 extends Node
 
+var player_in_range = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,6 +9,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if player_in_range and Input.is_action_just_pressed("interact"):
+		print("testing!")
 	pass
 
 
@@ -15,3 +18,14 @@ func _on_tp_to_room_0_1_body_entered(body: Node2D) -> void:
 	if body.name == "player":
 		get_tree().change_scene_to_file("res://rooms/room_1.tscn")
 	pass 
+
+
+func _on_npc_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		player_in_range = true
+		$npc/Label.visible = true
+
+func _on_npc_body_exited(body: Node2D) -> void:
+	if body.name == "player":
+		player_in_range = false
+		$npc/Label.visible = false
