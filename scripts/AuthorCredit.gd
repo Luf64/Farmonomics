fdbd6bad9b80078ccd91c07125a50bdf12db53ed
Phua Yew Zhian <yewzhianphua@gmail.com>
2026-04-29 18:30:49 +0800

    1) Songs and Credit: Added songs with creator approved to use 2) Removed the apple and orange item, and replaced the plant item with grass, as they are not needed in the game anymore. 3) Updated room_1.tscn to reflect the changes in the assets, and added a new scene for the grass environment. 4) Added a new script and scene to give credit to the creators of the songs used in the game.

---
+++ scripts/AuthorCredit.gd
@@ -0,0 +1,21 @@
+
+'''
+Credit 1
+Song: waera - harinezumi
+Music provided by NoCopyrightSounds
+Free Download/Stream: http://ncs.io/harinezumi
+Watch: http://ncs.lnk.to/harinezumiAT/youtube
+
+Credit 2
+Song: Egzod, Maestro Chives, Neoni - Royalty [NCS Release]
+Music provided by NoCopyrightSounds
+Free Download/Stream: http://ncs.io/Royalty
+Watch: http://ncs.lnk.to/RoyaltyAT/youtube
+
+Credit 3
+Song: Diviners & Ren - Savannah (Japanese version)
+Music provided by NoCopyrightSounds
+Free Download/Stream: http://ncs.io/savannahjapanese
+Watch: http://ncs.lnk.to/savannahjapaneseAT/youtube
+
+'''
--- scripts/room_1.gd
+++ scripts/room_1.gd
@@ -1,8 +1,9 @@
 extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if name == "Farm Door":
-		Global.room = "farm"
-		get_tree().change_scene_to_file(Global.Room0_1)
+		pass
+		'''Global.room = "farm"
+		get_tree().change_scene_to_file(Global.Room0_1)'''
 	elif name == "Room3 Door":
 		Global.room = "room3"
 		get_tree().change_scene_to_file(Global.Room_3)
