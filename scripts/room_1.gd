1a15890eb264f44d9b20d0c8eb7fdf72299194a3
Phua Yew Zhian <yewzhianphua@gmail.com>
2026-04-29 21:22:38 +0800

    Map added, Market place house added, y-sort adjusted, doors adjusted, music added

--- scripts/room_1.gd
+++ scripts/room_1.gd
@@ -1,9 +1,8 @@
 extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if name == "Farm Door":
-		pass
-		'''Global.room = "farm"
-		get_tree().change_scene_to_file(Global.Room0_1)'''
+		Global.room = "farm"
+		get_tree().change_scene_to_file(Global.Room0_1)
 	elif name == "Room3 Door":
 		Global.room = "room3"
 		get_tree().change_scene_to_file(Global.Room_3)
