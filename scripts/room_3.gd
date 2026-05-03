f66c62081c2c23805042d2ee5cadc42568497ed2
Aw Wei Jun <weijunaw@gmail.com>
2026-05-04 03:31:05 +0800

    (room 3 crafting and selling ui complete with a few bugs havent resolve (no function))

--- scripts/room_3.gd
+++ scripts/room_3.gd
@@ -8,6 +8,10 @@ var crafting_ui_open = false
 var crafting_ui = null
 var crafting_ui_room = preload("res://rooms/crafting_ui.tscn")
 
+var selling_ui_open = false
+var selling_ui = null
+var selling_ui_room = preload("res://rooms/shop_ui_room3.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -19,6 +23,9 @@ func _physics_process(delta: float) -> void:
 	
 	if player_in_range_tp_room1 and Input.is_action_just_pressed("interaction"):
 		get_tree().change_scene_to_file(Global.Room_1)
+	
+	if interaction_menu and Input.is_action_just_pressed("1"):
+		selling_ui_toggle()
 	
 	if interaction_menu and Input.is_action_just_pressed("2"):
 		crafting_ui_toggle()
@@ -64,3 +71,13 @@ func crafting_ui_toggle():
 		crafting_ui.queue_free()
 		crafting_ui = null
 		crafting_ui_open = false
+
+func selling_ui_toggle():
+	if not selling_ui_open:
+		selling_ui = selling_ui_room.instantiate()
+		call_deferred("add_child", selling_ui)
+		selling_ui_open = true
+	else:
+		selling_ui.queue_free()
+		selling_ui = null
+		selling_ui_open = false
