b7943be7ed2bfe9f2c98ab1bf587322a3d785949
Aw Wei Jun <weijunaw@gmail.com>
2026-05-03 21:57:13 +0800

    crafting ui is done but no function

--- scripts/crafting_ui.gd
+++ scripts/crafting_ui.gd
@@ -1,5 +1,4 @@
-extends Node
-
+class_name crafting_ui extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +8,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_texture_button_pressed() -> void:
+	var room = get_parent()
+	room.crafting_ui_open = false
+	room.crafting_ui = null
+	queue_free()
+	pass # Replace with function body.
