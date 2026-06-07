b6a5aeb4db399d4038554260c928f4d18be0c142
Aw Wei Jun <weijunaw@gmail.com>
2026-06-07 18:46:47 +0800

    testing inventory

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -5,26 +5,6 @@ extends CanvasLayer
 
 func _ready() -> void:
 	add_to_group("inventory_ui")
-	visible = false
 
 func _on_texture_button_pressed():
 	Global.open_inventory()
-
-'''
-func refresh_inventory():
-	container.clear()
-	
-	var items = json.get_inventory()
-	
-	for item in items:
-		var label = Label.new()
-		var text = item["id"] + " x" + str(item["amount"])
-		container.add_child(label)
-		label.text = text
-		container.add_child(label)
-
-
-func open_inventory():
-	refresh_inventory()
-	visible = true
-'''
