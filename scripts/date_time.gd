be5dfefa975a34bf7b19287d61ffede1c12715d5
Aw Wei Jun <weijunaw@gmail.com>
2026-06-13 01:54:45 +0800

    setting almost done with one bug

--- scripts/date_time.gd
+++ scripts/date_time.gd
@@ -11,34 +11,34 @@ var minute: int = 0 # 0-59
 # Month and Date structuring in Year Structure 
 
 static func month_name(x:int):
-    const Month_Name:= ["", "January","February","March","April","May","June","July","August","September","October","November","December"]
-    return Month_Name[clamp(x,1,12)]
+	const Month_Name:= ["", "January","February","March","April","May","June","July","August","September","October","November","December"]
+	return Month_Name[clamp(x,1,12)]
 
 func date():
-    var date = "%d %s %d" % [day, month_name(month), year]
-    return date
+	var date = "%d %s %d" % [day, month_name(month), year]
+	return date
 
 func time():
-    var time = "%02d:%02d:%02d" % [hour, minute,second]
-    return time
+	var time = "%02d:%02d:%02d" % [hour, minute,second]
+	return time
 
 func to_unix() -> int: # 1 Jan 1970
-    var dict:={
-        "year": year,
-        "month": month,
-        "day":day,
-        "hour":hour,
-        "minute": minute,
-        "second": second
-    }
-    return Time.get_unix_time_from_datetime_dict(dict)
+	var dict:={
+		"year": year,
+		"month": month,
+		"day":day,
+		"hour":hour,
+		"minute": minute,
+		"second": second
+	}
+	return Time.get_unix_time_from_datetime_dict(dict)
 
 func from_unix(unix:int) -> void:
-    var dictionary:=Time.get_datetime_dict_from_unix_time(unix)
-    year = dictionary["year"]
-    month = dictionary["month"]
-    day = dictionary["day"]
-    hour = dictionary["hour"]
-    minute = dictionary["minute"]
-    second = dictionary["second"]
-    
+	var dictionary:=Time.get_datetime_dict_from_unix_time(unix)
+	year = dictionary["year"]
+	month = dictionary["month"]
+	day = dictionary["day"]
+	hour = dictionary["hour"]
+	minute = dictionary["minute"]
+	second = dictionary["second"]
+	
