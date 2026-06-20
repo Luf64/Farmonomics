dc68f41c4d5055e57e54f99174aacaeff1c3f5eb
Aw Wei Jun <weijunaw@gmail.com>
2026-06-20 23:25:55 +0800

    bug fix for setting opening

--- scripts/setting.gd
+++ scripts/setting.gd
@@ -27,7 +27,6 @@ var sound_textures = {
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_to_group("setting")
-	Json.load_game()
 	refresh_ui()
 
 func _on_help_pressed() -> void:
@@ -52,66 +51,70 @@ func _on_sound_pressed() -> void:
 
 func set_volume_level(level: int) -> void:
 	level = clamp(level, 0, 10)
-
 	Global.sound_percent = level * 10.0
-	if level > 0:
-		Global.is_muted = false
 	for i in range(buttons.size()):
 		if i < level:
 			buttons[i].texture_normal = bar_textures["on"]
 		else:
 			buttons[i].texture_normal = bar_textures["off"]
+	sound.texture_normal = sound_textures["off"] if Global.is_muted else sound_textures["on"]
 	Global.apply_volume()
 
 func _on_1_pressed() -> void:
+	Global.is_muted = false
 	set_volume_level(1)
 	Json.save_game()
 
 func _on_2_pressed() -> void:
+	Global.is_muted = false
 	set_volume_level(2)
 	Json.save_game()
 
 func _on_3_pressed() -> void:
+	Global.is_muted = false
 	set_volume_level(3)
 	Json.save_game()
 
 func _on_4_pressed() -> void:
+	Global.is_muted = false
 	set_volume_level(4)
 	Json.save_game()
 
 func _on_5_pressed() -> void:
+	Global.is_muted = false
 	set_volume_level(5)
 	Json.save_game()
 
 func _on_6_pressed() -> void:
+	Global.is_muted = false
 	set_volume_level(6)
 	Json.save_game()
 
 func _on_7_pressed() -> void:
+	Global.is_muted = false
 	set_volume_level(7)
 	Json.save_game()
 
 func _on_8_pressed() -> void:
+	Global.is_muted = false
 	set_volume_level(8)
 	Json.save_game()
 
 func _on_9_pressed() -> void:
+	Global.is_muted = false
 	set_volume_level(9)
 	Json.save_game()
 
 func _on_10_pressed() -> void:
+	Global.is_muted = false
 	set_volume_level(10)
 	Json.save_game()
 
 func refresh_ui() -> void:
-	print("Refreshing UI")
-	print(Global.sound_percent)
-	print(Global.is_muted)
 	set_volume_level(Global.sound_percent / 10)
 	sound.texture_normal = sound_textures["off"] if Global.is_muted else sound_textures["on"]
 
 func _on_close_button_pressed() -> void:
-	Global.setting_open = false
-	Global.setting = null
+	Global.current_popup_room0 = null
 	queue_free()
 	pass # Replace with function body.
