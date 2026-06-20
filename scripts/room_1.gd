d6efb9397374405596cd93160976348f6aedcf80
Phua Yew Zhian <yewzhianphua@gmail.com>
2026-06-21 04:11:43 +0800

    Resolve: Merge Conflict

--- scripts/room_1.gd
+++ scripts/room_1.gd
@@ -1,25 +1,25 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "player":
-		if name == "Farm Door":
-			Global.current_room = "farm"
-			get_tree().change_scene_to_file(Global.Room0_1)
-		elif name == "Room3 Door":
-			Global.current_room = "room3"
-			get_tree().change_scene_to_file(Global.Room_3)
-		elif name == "Room2 Door":
-			Global.current_room = "room2"
-			get_tree().change_scene_to_file(Global.Room_2)
-		elif name == "lift door":
-			$Panel.visible = true
-			if Input.is_action_just_pressed("interaction"):
-				Global.current_room = "lift"
-				
-			
+    if body.name == "player":
+        if name == "Farm Door":
+            Global.current_room = "farm"
+            get_tree().change_scene_to_file(Global.Room0_1)
+        elif name == "Room3 Door":
+            Global.current_room = "room3"
+            get_tree().change_scene_to_file(Global.Room_3)
+        elif name == "Room2 Door":
+            Global.current_room = "room2"
+            get_tree().change_scene_to_file(Global.Room_2)
+        elif name == "lift door":
+            $Panel.visible = true
+            if Input.is_action_just_pressed("interaction"):
+                Global.current_room = "lift"
+                
+            
 
 
 func _on_body_exited(body: Node2D) -> void:
-	if body.name == "player":
-		if name == "lift door":
-			$Panel.visible = false
+    if body.name == "player":
+        if name == "lift door":
+            $Panel.visible = false
