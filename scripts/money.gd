f1867dff4c80b6945e6fcf03b540e58a6c98dac3
Phua Yew Zhian <yewzhianphua@gmail.com>
2026-06-24 02:48:48 +0800

    Fix and Add: Grass, Font Size, Transformation, Interaction, Room3 Background

--- scripts/money.gd
+++ scripts/money.gd
@@ -3,9 +3,9 @@ extends Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Global.money_changed.connect(_on_money_changed)
-	_on_money_changed(Global.money)
-	pass # Replace with function body.
+    Global.money_changed.connect(_on_money_changed)
+    _on_money_changed(Global.money)
+    pass # Replace with function body.
 
 func _on_money_changed(new_amount):
-	text = str(new_amount)
+    text = str(new_amount)
