9ca78f3330ddd081e54f278a91eabe3c34b6d5f3
Aw Wei Jun <weijunaw@gmail.com>
2026-06-27 23:14:50 +0800

    selling ui update and solve small ui

--- scripts/Room0.1.gd
+++ scripts/Room0.1.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
-    if body.name == "player":
-        Global.current_room = "room1"
-        get_tree().current_scene.save_all_plant()
-        get_tree().change_scene_to_file(Global.Room_1)
+	if body.name == "player":
+		Global.current_room = "room1"
+		get_tree().current_scene.save_all_plant()
+		get_tree().change_scene_to_file(Global.Room_1)
--- scripts/flower_spawn.gd
+++ scripts/flower_spawn.gd
@@ -11,37 +11,37 @@ var spawn_timer: float = 0.0
 var time_until_next_spawn: float = 0.0
 
 func _ready():
-    time_until_next_spawn = randf_range(spawn_interval_min, spawn_interval_max)
+	time_until_next_spawn = randf_range(spawn_interval_min, spawn_interval_max)
 
 func _physics_process(delta:float) -> void:
-    for i in range(active_flowers.size()-1,-1,-1):
-        if not is_instance_valid(active_flowers[i]):
-            active_flowers.remove_at(i)
-    spawn_timer += delta
-    if spawn_timer >= time_until_next_spawn:
-        if active_flowers.size() < max_flowers:
-            spawn_flowers()
-        spawn_timer = 0.0
-        time_until_next_spawn = randf_range(spawn_interval_min, spawn_interval_max)
-    
+	for i in range(active_flowers.size()-1,-1,-1):
+		if not is_instance_valid(active_flowers[i]):
+			active_flowers.remove_at(i)
+	spawn_timer += delta
+	if spawn_timer >= time_until_next_spawn:
+		if active_flowers.size() < max_flowers:
+			spawn_flowers()
+		spawn_timer = 0.0
+		time_until_next_spawn = randf_range(spawn_interval_min, spawn_interval_max)
+	
 func spawn_flowers() -> void:
-    var flowers_to_spawn = min(flowers_per_spawn,max_flowers-active_flowers.size())
-    for i in range(flowers_to_spawn):
-        var flower = flower_scene.instantiate()
-        add_child(flower)
-        flower.global_position = get_random_spawn_position()
-        active_flowers.append(flower)
+	var flowers_to_spawn = min(flowers_per_spawn,max_flowers-active_flowers.size())
+	for i in range(flowers_to_spawn):
+		var flower = flower_scene.instantiate()
+		add_child(flower)
+		flower.global_position = get_random_spawn_position()
+		active_flowers.append(flower)
 
 func get_random_spawn_position() -> Vector2:
-    var collision_shape = $CollisionShape2D
-    var shape = collision_shape.shape
-    if shape is RectangleShape2D:
-        var rectangle = shape.size
-        var random_x = randf_range(-rectangle.x/2,rectangle.x/2)
-        var random_y = randf_range(-rectangle.y/2,rectangle.y/2)
-        return global_position +Vector2(random_x,random_y)
-    return global_position
+	var collision_shape = $CollisionShape2D
+	var shape = collision_shape.shape
+	if shape is RectangleShape2D:
+		var rectangle = shape.size
+		var random_x = randf_range(-rectangle.x/2,rectangle.x/2)
+		var random_y = randf_range(-rectangle.y/2,rectangle.y/2)
+		return global_position +Vector2(random_x,random_y)
+	return global_position
 
 
 func _on_flower_plant_region_visibility_changed() -> void:
-        set_physics_process(visible)
+		set_physics_process(visible)
