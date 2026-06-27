a06338f4d98cff94cafacd4b3dc0d58007490d6d
Phua Yew Zhian <yewzhianphua@gmail.com>
2026-06-27 21:49:18 +0800

    Bug

--- scripts/room_0.gd
+++ scripts/room_0.gd
@@ -3,38 +3,38 @@ extends Node2D
 
 
 func _on_continue_pressed() -> void:
-	Json.load_game()
-	Global.hotbar_ui.visible =! Global.hotbar_ui.visible
-	get_tree().change_scene_to_file(Global.Room_1)
+    Json.load_game()
+    Global.hotbar_ui.visible =! Global.hotbar_ui.visible
+    get_tree().change_scene_to_file(Global.Room_1)
 
 var newgame = preload("res://rooms/user_sign_in.tscn")
 func _on_new_game_pressed() -> void:
-	Json.game["money"] = 100
-	Json.game["inventory"] = [
-		{},{},{},{},{},
-		{},{},{},{},{},
-		{},{},{},{},{},
-		{},{},{},{},{},
-		{},{},{},{},{},
-		{},{},{},{},{},
-	]
-	Json.game["scene"] = Global.Room_1
-	Global.money = 100
-	Json.save_game()
-	Global.inventory_ui.refresh()
-	Global.hotbar_ui.refresh()
-	Global.open_popup_room0(newgame)
+    Json.game["money"] = 100
+    Json.game["inventory"] = [
+        {},{},{},{},{},
+        {},{},{},{},{},
+        {},{},{},{},{},
+        {},{},{},{},{},
+        {},{},{},{},{},
+        {},{},{},{},{},
+    ]
+    Json.game["scene"] = Global.Room_1
+    Global.money = 100
+    Json.save_game()
+    Global.inventory_ui.refresh()
+    Global.hotbar_ui.refresh()
+    Global.open_popup_room0(newgame)
 
 func _on_settings_pressed() -> void:
-	Global.open_popup_room0(Global.setting_room)
-	pass # Replace with function body.
+    Global.open_popup_room0(Global.setting_room)
+    pass # Replace with function body.
 
 var quit_page = preload("res://rooms/quit_game.tscn")
 
 func _on_quit_game_pressed() -> void:
-	Global.open_popup_room0(quit_page)
+    Global.open_popup_room0(quit_page)
 
 var credits_page = preload("res://rooms/Credit.tscn")
 
 func _on_credits_pressed() -> void:
-	Global.open_popup_room0(credits_page)
+    Global.open_popup_room0(credits_page)
