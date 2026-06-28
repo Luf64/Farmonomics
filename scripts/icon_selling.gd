2c3e856f4923faad3b528041c24094f0e092d5c1
Phua Yew Zhian <yewzhianphua@gmail.com>
2026-06-29 02:40:15 +0800

    Deleted 2 useless picture

--- scripts/icon_selling.gd
+++ scripts/icon_selling.gd
@@ -6,6 +6,6 @@ signal sell_pressed(slot_number: int)
 var sellable: bool = false
 
 func _on_icon_pressed() -> void:
-	print("Clicked slot ", slot_number, " sellable=", sellable)
-	if sellable:
-		sell_pressed.emit(slot_number)
+    print("Clicked slot ", slot_number, " sellable=", sellable)
+    if sellable:
+        sell_pressed.emit(slot_number)
