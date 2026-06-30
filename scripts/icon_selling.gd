b55525df58d6e4a63a3cce7caaaf58bd847ce2f9
Phua Yew Zhian <yewzhianphua@gmail.com>
2026-06-30 16:59:22 +0800

    File Arrangement: Deleted Assets

--- scripts/icon_selling.gd
+++ scripts/icon_selling.gd
@@ -7,9 +7,9 @@ var sellable: bool = false
 
 func _on_icon_pressed() -> void:
 
-	print("Clicked slot ", slot_number, " sellable=", sellable)
-	if sellable:
-		sell_pressed.emit(slot_number)
+    print("Clicked slot ", slot_number, " sellable=", sellable)
+    if sellable:
+        sell_pressed.emit(slot_number)
 
 func set_price(value: int) -> void:
     $price.text = str(value) if value > 0 else ""
