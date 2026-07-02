00c8aef8b259317fe71fd41f4e5d0d249463ba0c
Phua Yew Zhian <yewzhianphua@gmail.com>
2026-07-03 01:14:01 +0800

    Bug fixed: Flower state at night

--- scripts/bedroom.gd
+++ scripts/bedroom.gd
@@ -4,42 +4,42 @@ extends Node2D
 var player_in_range: bool = false
 
 func trigger_sleep() -> void:
-	if not Timemanager.can_player_sleep():
-		print("not tired enough to sleep yet")
-		return
+    if not Timemanager.can_player_sleep():
+        print("not tired enough to sleep yet")
+        return
 
-	print("sleeping...")
-	get_tree().call_group("TimeSystem", "player_sleep")
+    print("sleeping...")
+    get_tree().call_group("TimeSystem", "player_sleep")
 
-	if prompt:
-		prompt.visible = false
+    if prompt:
+        prompt.visible = false
 
 func _process(_delta: float) -> void:
-	if player_in_range and Input.is_action_just_pressed("interaction"):
-		trigger_sleep()
+    if player_in_range and Input.is_action_just_pressed("interaction"):
+        trigger_sleep()
 
 
 func _on_area_2d_3_body_entered(body: Node2D) -> void:
-	if body.name == "player" or body.is_in_group("player"):
-		player_in_range = true
-		if prompt:
-			prompt.visible = true 
-	pass # Replace with function body.
+    if body.name == "player" or body.is_in_group("player"):
+        player_in_range = true
+        if prompt:
+            prompt.visible = true 
+    pass # Replace with function body.
 
 
 func _on_area_2d_3_body_exited(body: Node2D) -> void:
-	if body.name == "player" or body.is_in_group("player"):
-		player_in_range = false
-		if prompt:
-			prompt.visible = false
-	pass # Replace with function body.
+    if body.name == "player" or body.is_in_group("player"):
+        player_in_range = false
+        if prompt:
+            prompt.visible = false
+    pass # Replace with function body.
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "player":
-		Json.save_game()
-		Global.current_room = "Bedroom"
-		Global.coordinates = "Farm"
-		get_tree().change_scene_to_file(Global.Room_1)
-		Json.load_game()
-	pass # Replace with function body.
+    if body.name == "player":
+        Json.save_game()
+        Global.current_room = "Bedroom"
+        Global.coordinates = "Farm"
+        get_tree().change_scene_to_file(Global.Room_1)
+        Json.load_game()
+    pass # Replace with function body.
--- scripts/global.gd
+++ scripts/global.gd
@@ -23,15 +23,15 @@ var player: Node = null
 var current_room: String = ""
 var coordinates: String = ""
 var seed_to_product: Dictionary = {
-	"Seed_corn": "Corn",
-	"Seed_chocolate": "Chocolate",
-	"Seed_milk": "Milk"
+    "Seed_corn": "Corn",
+    "Seed_chocolate": "Chocolate",
+    "Seed_milk": "Milk"
 }
 
 var grow_time: Dictionary = {
-	"Seed_corn": 10.0,
-	"Seed_chocolate": 20.0,
-	"Seed_milk": 30.0
+    "Seed_corn": 10.0,
+    "Seed_chocolate": 20.0,
+    "Seed_milk": 30.0
 }
 
 var setting_room = preload("res://rooms/setting.tscn")
@@ -48,178 +48,178 @@ var time = {}
 var player_name: String = ""
 
 var money: int = 100:
-	set(value):
-		money = value
-		money_changed.emit(money)
+    set(value):
+        money = value
+        money_changed.emit(money)
 
 var room = {
-	"farm": {"scene": Room0_1, "coordinates": "Farm"},
-	"room1": {"scene": Room_1, "coordinates": "Room1"},
-	"room2": {"scene": Room_2, "coordinates": "Room2"},
-	"room3": {"scene": Room_3, "coordinates": "Room3"},
-	"sky_shop": {"scene": sky_shop, "coordinates": "SkyShop"},
+    "farm": {"scene": Room0_1, "coordinates": "Farm"},
+    "room1": {"scene": Room_1, "coordinates": "Room1"},
+    "room2": {"scene": Room_2, "coordinates": "Room2"},
+    "room3": {"scene": Room_3, "coordinates": "Room3"},
+    "sky_shop": {"scene": sky_shop, "coordinates": "SkyShop"},
 }
 
 var crops = {
-	"Corn": {
-		"grow_time": 10,
-		"base_price": 1,
-		"current_price": 1,
-		"stock": 25,
-		"ideal": 25,
-		"rarity":1
-	},
-	"Chocolate": {
-		"grow_time": 20,
-		"base_price": 1,
-		"current_price": 1,
-		"stock": 20,
-		"ideal": 20,
-		"rarity": 2
-	},
-	"Milk": {
-		"grow_time": 30,
-		"base_price": 1,
-		"current_price": 1,
-		"stock": 15,
-		"ideal": 15,
-		"rarity": 3
-	},
-	"Orange": {
-		"grow_time": 40,
-		"base_price": 1,
-		"current_price": 1,
-		"stock": 10,
-		"ideal": 10,
-		"rarity":4
-	},
-	"Tomato": {
-		"grow_time": 40,
-		"base_price": 1,
-		"current_price": 1,
-		"stock": 5,
-		"ideal": 5,
-		"rarity":5
-	}
+    "Corn": {
+        "grow_time": 10,
+        "base_price": 1,
+        "current_price": 1,
+        "stock": 25,
+        "ideal": 25,
+        "rarity":1
+    },
+    "Chocolate": {
+        "grow_time": 20,
+        "base_price": 1,
+        "current_price": 1,
+        "stock": 20,
+        "ideal": 20,
+        "rarity": 2
+    },
+    "Milk": {
+        "grow_time": 30,
+        "base_price": 1,
+        "current_price": 1,
+        "stock": 15,
+        "ideal": 15,
+        "rarity": 3
+    },
+    "Orange": {
+        "grow_time": 40,
+        "base_price": 1,
+        "current_price": 1,
+        "stock": 10,
+        "ideal": 10,
+        "rarity":4
+    },
+    "Tomato": {
+        "grow_time": 40,
+        "base_price": 1,
+        "current_price": 1,
+        "stock": 5,
+        "ideal": 5,
+        "rarity":5
+    }
 }
 var seed ={
-	"Seed_corn": {
-		"base_price": 1,
-		"current_price": 1,
-		"stock": 40,
-		"ideal": 60,
-		"rarity":1
-	},
-	"Seed_chocolate": {
-		"base_price": 5,
-		"current_price": 5,
-		"stock": 25,
-		"ideal": 50,
-		"rarity": 2
-	},
-	"Seed_milk": {
-		"base_price": 10,
-		"current_price": 10,
-		"stock": 15,
-		"ideal": 50,
-		"rarity": 3
-	}
+    "Seed_corn": {
+        "base_price": 1,
+        "current_price": 1,
+        "stock": 40,
+        "ideal": 60,
+        "rarity":1
+    },
+    "Seed_chocolate": {
+        "base_price": 5,
+        "current_price": 5,
+        "stock": 25,
+        "ideal": 50,
+        "rarity": 2
+    },
+    "Seed_milk": {
+        "base_price": 10,
+        "current_price": 10,
+        "stock": 15,
+        "ideal": 50,
+        "rarity": 3
+    }
 }
 
 var flower = {
-	"Flower_Red": {
-		"base_price": 1,
-		"current_price": 1,
-		"stock": 5,
-		"ideal": 5,
-		"rarity":1
-	},
-	"Flower_Blue": {
-		"base_price": 1,
-		"current_price": 1,
-		"stock": 5,
-		"ideal": 5,
-		"rarity":1
-	},
-	"Flower_Yellow": {
-		"base_price": 1,
-		"current_price": 1,
-		"stock": 5,
-		"ideal": 5,
-		"rarity":1
-	},
-	"Flower_Purple": {
-		"base_price": 1,
-		"current_price": 1,
-		"stock": 5,
-		"ideal": 5,
-		"rarity":1
-	},
-	"Flower_White": {
-		"base_price": 1,
-		"current_price": 1,
-		"stock": 5,
-		"ideal": 5,
-		"rarity":1
-	}
+    "Flower_Red": {
+        "base_price": 1,
+        "current_price": 1,
+        "stock": 5,
+        "ideal": 5,
+        "rarity":1
+    },
+    "Flower_Blue": {
+        "base_price": 1,
+        "current_price": 1,
+        "stock": 5,
+        "ideal": 5,
+        "rarity":1
+    },
+    "Flower_Yellow": {
+        "base_price": 1,
+        "current_price": 1,
+        "stock": 5,
+        "ideal": 5,
+        "rarity":1
+    },
+    "Flower_Purple": {
+        "base_price": 1,
+        "current_price": 1,
+        "stock": 5,
+        "ideal": 5,
+        "rarity":1
+    },
+    "Flower_White": {
+        "base_price": 1,
+        "current_price": 1,
+        "stock": 5,
+        "ideal": 5,
+        "rarity":1
+    }
 }
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var inventory_room = load(Inventory)
-	inventory_ui = inventory_room.instantiate()
-	get_tree().root.call_deferred("add_child", inventory_ui)
-	inventory_ui.visible = false
-	var hotbar_room = load("res://rooms/hotbar.tscn")
-	hotbar_ui = hotbar_room.instantiate()
-	get_tree().root.call_deferred("add_child", hotbar_ui)
-	hotbar_ui.visible = false
-	Json.load_game()
-	apply_volume()
+    var inventory_room = load(Inventory)
+    inventory_ui = inventory_room.instantiate()
+    get_tree().root.call_deferred("add_child", inventory_ui)
+    inventory_ui.visible = false
+    var hotbar_room = load("res://rooms/hotbar.tscn")
+    hotbar_ui = hotbar_room.instantiate()
+    get_tree().root.call_deferred("add_child", hotbar_ui)
+    hotbar_ui.visible = false
+    Json.load_game()
+    apply_volume()
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_cancel"):
-		if current_popup_room0 != null:
-			current_popup_room0.queue_free()
-			current_popup_room0 = null
-		else:
-			current_popup_room0 = setting_room.instantiate()
-			get_tree().root.add_child(current_popup_room0)
+    if event.is_action_pressed("ui_cancel"):
+        if current_popup_room0 != null:
+            current_popup_room0.queue_free()
+            current_popup_room0 = null
+        else:
+            current_popup_room0 = setting_room.instantiate()
+            get_tree().root.add_child(current_popup_room0)
 
 #open inventory
 func open_inventory() -> void:
-	inventory_ui.visible = !inventory_ui.visible
+    inventory_ui.visible = !inventory_ui.visible
 
 #inventory JSON system
 #player status global/local
 
 func subtract_money(amount: int) -> bool:
-	if money >= amount:
-		money -= amount
-		money_changed.emit(money)
-		return true
-	else:
-		return false
-		
+    if money >= amount:
+        money -= amount
+        money_changed.emit(money)
+        return true
+    else:
+        return false
+        
 
 func apply_volume():
-	var bus = AudioServer.get_bus_index("Master")
-	
-	AudioServer.set_bus_mute(bus, is_muted)
-	
-	if is_muted:
-		return
-	
-	var db = linear_to_db(sound_percent / 100.0)
-	AudioServer.set_bus_volume_db(bus, db)
+    var bus = AudioServer.get_bus_index("Master")
+    
+    AudioServer.set_bus_mute(bus, is_muted)
+    
+    if is_muted:
+        return
+    
+    var db = linear_to_db(sound_percent / 100.0)
+    AudioServer.set_bus_volume_db(bus, db)
 
 func open_popup_room0(scene: PackedScene):
-	# if something already open → close it first
-	if current_popup_room0 != null:
-		current_popup_room0.queue_free()
-		current_popup_room0 = null
+    # if something already open → close it first
+    if current_popup_room0 != null:
+        current_popup_room0.queue_free()
+        current_popup_room0 = null
 
-	# open new popup
-	current_popup_room0 = scene.instantiate()
-	get_tree().root.add_child(current_popup_room0)
+    # open new popup
+    current_popup_room0 = scene.instantiate()
+    get_tree().root.add_child(current_popup_room0)
